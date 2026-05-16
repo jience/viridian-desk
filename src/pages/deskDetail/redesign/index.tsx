117d202c39f17196afa3359c52156d1d8fabdbeb
Alex <zalex2015@hotmail.com>
2026-05-16 09:51:59 +0800

    Add redesigned desktop detail bridge

---
+++ src/pages/deskDetail/redesign/index.tsx
@@ -0,0 +1,10 @@
+import { Component as LegacyDeskDetail } from '..';
+import './index.scss';
+
+export function Component() {
+  return (
+    <div className="redesign-desk-detail-page">
+      <LegacyDeskDetail />
+    </div>
+  );
+}
