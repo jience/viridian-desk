e29007bb6fecb20cb021dab75d112e99f6cbcbbd
Alex <zalex2015@hotmail.com>
2026-05-17 18:33:53 +0800

    Polish legacy table controls

--- src/components/TableBtn/index.tsx
+++ src/components/TableBtn/index.tsx
@@ -4,9 +4,8 @@ const TableBtn = (props: any) => {
   const { icon, className, ...rest } = props;
   return (
     <Button
-      className={`${className} tableBtnInit`}
+      className={[className, 'tableBtnInit'].filter(Boolean).join(' ')}
       icon={<i className={`iconfont ${icon}`} />}
-      style={{ marginRight: 5 }}
       {...rest}
     />
   );
