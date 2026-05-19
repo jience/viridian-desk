f5f4270f39af47f323165e2dd54e59f22e00964f
Alex <zalex2015@hotmail.com>
2026-05-19 08:50:35 +0800

    Replace dark layout image with CSS background

--- src/components/DeskLoading/index.tsx
+++ src/components/DeskLoading/index.tsx
@@ -1,11 +1,8 @@
 import './index.scss';
 import { useMemo, type FC } from 'react';
 import deskEnterLoading from '@/assets/images/deskEnterLoading.gif';
-import app_layout_bg_dark from '@/assets/images/app_layout_bg_dark.png';
-import app_layout_bg from '@/assets/images/app_layout_bg.png';
 import { useAppSelector } from '@/store';
 import { selectBackgroundImage } from '@/store/feature/client';
-import { selectTheme } from '@/store/feature/config';
 
 export interface DeskLoadingProps {
   text: string;
@@ -14,19 +11,14 @@ export interface DeskLoadingProps {
 const DeskLoading: FC<DeskLoadingProps> = (props) => {
   const { text } = props;
   const backgroundImage = useAppSelector(selectBackgroundImage);
-  const theme = useAppSelector(selectTheme);
 
   const bgStyle = useMemo(() => {
     if (backgroundImage) {
       return {
         backgroundImage: `url(${backgroundImage})`,
       };
-    } else {
-      return {
-        backgroundImage: theme == 'dark' ? `url(${app_layout_bg_dark})})` : `url(${app_layout_bg})`,
-      };
     }
-  }, [theme, backgroundImage]);
+  }, [backgroundImage]);
 
   return (
     <div className="desk-loading-background-mask" style={bgStyle}>
